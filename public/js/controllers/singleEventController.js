43f0825cb3c6fcf106185bd6bba87b9d7f84f764
Nuria <soriano.nuria@gmail.com>
2016-08-25 14:26:38 +0200

    fix add to calendar button on details page

--- public/js/controllers/singleEventController.js
+++ public/js/controllers/singleEventController.js
@@ -1,27 +1,55 @@
 angular.module('singleEventController', [])
-	.controller( 'singleEventController' , function ( $scope, $routeParams, Event, Icons ) {
+	.controller( 'singleEventController' , function ( $scope, $rootScope, $routeParams, Event, User, Icons ) {
 		
+		//get event by the url parameter
 		var eventId = $routeParams.ID;
-
 		Event.getOne( eventId )
 			.then(function(dataEvent){
 				$scope.event = dataEvent.data;
 			})
 
+		// get icons to display
 		$scope.iconVisibilityName = function(event){
 			if(event){
 				return Icons.getIconVisibility(event);	
 			}			
 		};
-
 		$scope.iconCategoryName = function(event){
 			if(event){
 				return Icons.getIconCat(event);
 			}
 		};
 
-		$scope.addToCalendar = function(user){
-			alert('added to calendar '+user);
-		};
+		// check if the user is logged in and prepare an array with the events they have already saved
+		var user = $rootScope.user;
+		// $scope.user = user;
+
+		var addedItems = [];
+		if(user){
+			User.get(user)
+				.then(function(dataUser){
+					var dataUser = dataUser.data[0];
+					addedItems = dataUser.events
+				});
+		}
+		
+		// check if the event has been saved by the user to show or hide the Add to calendar button
+		$scope.added = function(){
+			console.log(eventId)
+			if(addedItems.indexOf(eventId) !== -1){
+				return true; //hide
+			} else {
+				return false; //show
+			}
+			
+		}
+
+		$scope.addToCalendar = function(){
+			// add the event to the database
+			User.addEvent(user, eventId)
+				.then(function(){
+					addedItems.push(eventId); // and store it in the saved events array
+				});
+		}
 		
 	});
