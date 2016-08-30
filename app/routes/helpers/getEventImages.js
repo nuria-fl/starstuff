99154362259a649bb5df3b0dd65c49330b27937d
Nuria <soriano.nuria@gmail.com>
2016-08-30 16:53:07 +0200

    show pics in home page

--- app/routes/helpers/getEventImages.js
+++ app/routes/helpers/getEventImages.js
@@ -1,10 +1,8 @@
 var Image = require('../../models/image');
-var mongoose = require('mongoose');
-var ObjectId = mongoose.Schema.Types.ObjectId;
 
 function getEventImages (req, res) {
 	var eventId = req.params.eventId;
-	Image.find({ 'event[0]._id': ObjectId(eventId) } ).exec(function(err, images) {
+	Image.find({ 'event._id': eventId } ).exec(function(err, images) {
 		if (err)
 			res.send(err);
 		res.json(images); // return all events in JSON format
--- app/routes/helpers/getImages.js
+++ app/routes/helpers/getImages.js
@@ -1,18 +1,10 @@
 var Image = require('../../models/image');
 
 function getImages (req, res) {
-	Image.find(function (err, doc) {
+	Image.find(function (err, images) {
 		if (err) return next(err);
-		var formattedArr = [];
-		doc.forEach(function(elem, i){
-			var imgObj = {};
-			var base64img = elem.img.data.toString('base64');
-			imgObj.title = 'title';
-			imgObj.user = elem.user;
-			imgObj.img = base64img;
-			formattedArr.push(imgObj);
-		})
-		res.json(formattedArr);
+		
+		res.json(images);
 	});
 }
 
